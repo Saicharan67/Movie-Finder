013f7edc844b08a4d55b42b626ac98868e43d44b
Saicharan67 <mahankalisaicharan@gmail.com>
2020-09-21 17:36:47 +0530

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Finder from "./components/Main.js/main.js";
+import Nav from "./pages/navigator.js";
+import Footer from "./pages/foooter.js";
 function App() {
-  return <Finder />;
+  return (
+    <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={() => <Finder />} />
+          <Route path="/Actor" exact component={() => <Finder />} />
+          <Route path="/Director" exact component={() => <Finder />} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
