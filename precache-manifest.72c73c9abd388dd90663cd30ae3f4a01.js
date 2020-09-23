19cb33d36e9d0621517767d1e4e6ce0e01864ee8
Saicharan67 <mahankalisaicharan@gmail.com>
2020-09-23 23:34:13 +0530

    Updates

---
+++ precache-manifest.72c73c9abd388dd90663cd30ae3f4a01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ebf59b8ada2e8e653072c0d01ae7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bfe26d74f248adce2da",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4111acfd3f34c4cddce1",
+    "url": "/static/css/main.af895d3e.chunk.css"
+  },
+  {
+    "revision": "0bfe26d74f248adce2da",
+    "url": "/static/js/2.3bd58479.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/static/js/2.3bd58479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4111acfd3f34c4cddce1",
+    "url": "/static/js/main.b35a62f6.chunk.js"
+  },
+  {
+    "revision": "09fede28addc8cc6503e",
+    "url": "/static/js/runtime-main.a702fe1d.js"
+  },
+  {
+    "revision": "4bc099bdcf5adc8d9c1f616143b6d489",
+    "url": "/static/media/Nodata.4bc099bd.jpg"
+  },
+  {
+    "revision": "0af8f612c1e18366fa9059beb56aa4be",
+    "url": "/static/media/gear.0af8f612.gif"
+  },
+  {
+    "revision": "34eb35caf647a944b43c86d7a4db0c56",
+    "url": "/static/media/loadin2.34eb35ca.gif"
+  }
+]);
