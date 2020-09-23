19bc97139d2ada336b7d6334aafe992518211302
Saicharan67 <mahankalisaicharan@gmail.com>
2020-09-23 23:42:50 +0530

    Updates

---
+++ precache-manifest.909d9ba21c69e10b86e204f22394a86e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34982cb09b8fc0c356ff285252b7144",
+    "url": "/Movie-Finder/index.html"
+  },
+  {
+    "revision": "0bfe26d74f248adce2da",
+    "url": "/Movie-Finder/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b364977d01049c257444",
+    "url": "/Movie-Finder/static/css/main.af895d3e.chunk.css"
+  },
+  {
+    "revision": "0bfe26d74f248adce2da",
+    "url": "/Movie-Finder/static/js/2.3bd58479.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/Movie-Finder/static/js/2.3bd58479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b364977d01049c257444",
+    "url": "/Movie-Finder/static/js/main.000df793.chunk.js"
+  },
+  {
+    "revision": "d7763e271d85fdba53d9",
+    "url": "/Movie-Finder/static/js/runtime-main.f0895b71.js"
+  },
+  {
+    "revision": "4bc099bdcf5adc8d9c1f616143b6d489",
+    "url": "/Movie-Finder/static/media/Nodata.4bc099bd.jpg"
+  },
+  {
+    "revision": "0af8f612c1e18366fa9059beb56aa4be",
+    "url": "/Movie-Finder/static/media/gear.0af8f612.gif"
+  },
+  {
+    "revision": "34eb35caf647a944b43c86d7a4db0c56",
+    "url": "/Movie-Finder/static/media/loadin2.34eb35ca.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72c73c9abd388dd90663cd30ae3f4a01.js"
+  "/Movie-Finder/precache-manifest.909d9ba21c69e10b86e204f22394a86e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Movie-Finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
