1bb176c58b913363883682b67e71943fb303034c
Saicharan67 <mahankalisaicharan@gmail.com>
2020-10-07 20:10:49 +0530

    changed error in imahe

--- src/components/Movies/movieclip.js
+++ src/components/Movies/movieclip.js
@@ -5,18 +5,25 @@ const MovieClip = (props) => {
   const myfuc = (event) => {
     props.when(props.id);
   };
+  const urlfinder = () => {
+    let url = "https://image.tmdb.org/t/p/w185" + props.poster;
+    // props.poster
+    // ? fetch("https://image.tmdb.org/t/p/w185" + props.poster)
+    // : require("../../assets/noimg.png")
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          url = "https://image.tmdb.org/t/p/w185" + props.poster;
+        }
+      })
+      .catch((err) => {
+        url = require("../../assets/noimg.png");
+      });
+    return url;
+  };
   return (
     <div className="clip">
-      <img
-        src={
-          props.poster
-            ? "https://image.tmdb.org/t/p/w185" + props.poster
-            : require("../../assets/noimg.png")
-        }
-        alt="poster"
-        className="img"
-        onClick={myfuc}
-      />
+      <img src={urlfinder()} alt="poster" className="img" onClick={myfuc} />
       <p>{props.Name}</p>
     </div>
   );
