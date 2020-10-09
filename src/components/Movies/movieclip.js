047df49fb4cb0144faddbd2f76372cacad79e7a0
Saicharan67 <mahankalisaicharan@gmail.com>
2020-10-09 12:06:40 +0530

    solved issue

--- src/components/Movies/movieclip.js
+++ src/components/Movies/movieclip.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const MovieClip = (props) => {
+  const [Url, setUrl] = useState("");
   const myfuc = (event) => {
     props.when(props.id);
   };
   const urlfinder = () => {
-    let url = "https://image.tmdb.org/t/p/w185" + props.poster;
     // props.poster
     // ? fetch("https://image.tmdb.org/t/p/w185" + props.poster)
     // : require("../../assets/noimg.png")
-    fetch(url)
+
+    let imageurl = "https://image.tmdb.org/t/p/w185" + props.poster;
+
+    fetch(imageurl)
       .then((res) => {
         if (res.ok) {
-          url = "https://image.tmdb.org/t/p/w185" + props.poster;
+          setUrl("https://image.tmdb.org/t/p/original" + props.poster);
         }
       })
       .catch((err) => {
-        url = require("../../assets/noimg.png");
+        setUrl("");
       });
-    return url;
+    return Url;
   };
   return (
     <div className="clip">
-      <img src={urlfinder()} alt="poster" className="img" onClick={myfuc} />
+      <img
+        src={
+          urlfinder()
+            ? "https://image.tmdb.org/t/p/w185" + props.poster
+            : require("../../assets/noimg.png")
+        }
+        alt="poster"
+        className="img"
+        onClick={myfuc}
+      />
       <p>{props.Name}</p>
     </div>
   );
